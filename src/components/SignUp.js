0f5a6c4cb359773693cea1cb0bf1061a719d386c
ggsuccess <ggsuccess@naver.com>
2020-07-30 18:08:56 +0900

    signSEt

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -20,7 +20,7 @@ const SignUp = () => {
   return (
     <>
       <form onSubmit={} method="post">
-        <div>회원 가입</div>
+        <div title="회원 가입">회원 가입</div>
         아이디:
         <input
           type="text"
@@ -68,14 +68,49 @@ const SignUp = () => {
         </input>
         <br />
         생년월일:
-        <select name="birthdayYear">
-          <option value="">1970</option>
-          <option value="">1971</option>
-          <option value="">1972</option>
-          <option value="">1973</option>
-          <option value="">1974</option>
-          <option value="">1975</option>
+        <select name="birthdayYear" onChange={}>
+          <option label="" value="">
+            1970년
+          </option>
+          <option value="">1971년</option>
+          <option value="">1972년</option>
+          <option value="">1973년</option>
+          <option value="">1974년</option>
+          <option value="">1975년</option>
+          <option value="">1976년</option>
+          <option value="">1977년</option>
+          <option value="">1978년</option>
+          <option value="">1979년</option>
+          <option value="">1980년</option>
+          <option value="">1981년</option>
+          <option value="">1982년</option>
+          <option value="">1983년</option>
+          <option value="">1984년</option>
+          <option value="">1985년</option>
+          <option value="">1986년</option>
+          <option value="">1987년</option>
+          <option value="">1988년</option>
           <option value="">1976</option>
+          <option value="">1976</option>
+          <option value="">1976</option>
+          <option value="">1976</option>
+        </select>
+        <select name="birthdayMonth" onChange>
+          <option value={1}>1월</option>
+          <option value={2}>2월</option>
+          <option value={3}>3월</option>
+          <option value={4}>4월</option>
+          <option value={5}>5월</option>
+          <option value={6}>6월</option>
+          <option value={7}>7월</option>
+          <option value={8}>8월</option>
+          <option value={9}>9월</option>
+          <option value={10}>10월</option>
+          <option value={11}>11월</option>
+          <option value={12}>12월</option>
+        </select>
+        <select>
+          <option value="1일">1일</option>
         </select>
         <button type="submit">회원가입</button>
       </form>
